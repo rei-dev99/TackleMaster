913e8628d5e9b6d5bf6d23a5502e4f65eac78591
rei <idareiya123@gmail.com>
2024-05-05 20:21:50 +0900

    環境構築、トップページ表示完了

---
+++ config/application.rb
@@ -0,0 +1,32 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Tacklemaster
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+    config.generators do |g|
+      g.skip_routes true  #ルーティングを生成しない
+      g.helper false #helperを生成しない
+      g.test_framework false #testファイルを生成しない
+    end
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
