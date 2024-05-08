e1431184010e0f2b244ca30d1bc3c9e969dd66ce
rei <idareiya123@gmail.com>
2024-05-09 07:53:42 +0900

    render設定、微修正

--- app/controllers/tackles_controller.rb
+++ app/controllers/tackles_controller.rb
@@ -1,13 +1,25 @@
 class TacklesController < ApplicationController
   def index
-  end
 
-  def new
-  end
-
-  def edit
   end
 
   def show
+
+  end
+
+  def new
+    # @tackle = Tackle.new
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "tops#index"
   resources :users
+  resources :tackles
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
