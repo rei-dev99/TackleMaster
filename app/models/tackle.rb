38188137953f8dda6809aeba357b5689fb68a610
rei <idareiya123@gmail.com>
2024-05-09 14:35:05 +0900

    リールテーブル、中間テーブル

--- app/models/tackle.rb
+++ app/models/tackle.rb
@@ -2,4 +2,6 @@ class Tackle < ApplicationRecord
   belongs_to :user
   has_many :tackle_rods
   has_many :rods, through: :tackle_rods
+  has_many :tackles_reels
+  has_many :reels, through: :tackles_reels
 end
---
+++ app/models/tackle_reel.rb
@@ -0,0 +1,4 @@
+class TackleReel < ApplicationRecord
+  belongs_to :tackle
+  belongs_to :reel
+end
