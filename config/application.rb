bb8d7749f04fb07dc13e27832c47921a150e3373
rei <idareiya123@gmail.com>
2024-05-11 07:06:02 +0900

    タックル途中まで

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Tacklemaster
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
