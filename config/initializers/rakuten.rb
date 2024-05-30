477636072bc27eb0c95f4a836cf5eca961f4d588
rei <idareiya123@gmail.com>
2024-05-30 13:53:34 +0900

    楽天apiにapplication_id付与

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,1 +1,5 @@
-RWS_APPLICATION_ID = ENV['RWS_APPLICATION_ID']
+# RWS_APPLICATION_ID = ENV['RWS_APPLICATION_ID']
+
+RakutenWebService.configure do |c|
+    c.application_id = ENV['RWS_APPLICATION_ID'] # 環境変数から読み込む
+end
