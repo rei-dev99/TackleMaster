af71a4db4abbf6b8beb72e7e48f8bdeaaeeb82a0
rei <idareiya123@gmail.com>
2024-06-08 15:24:34 +0900

    OAuth_completion

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -7,29 +7,22 @@ class OauthsController < ApplicationController
   end
 
   def callback
-    # binding.pry
     provider = auth_params[:provider]
-    # Rails.logger.debug "Provider: #{provider}"
 
     if auth_params[:code].blank?
-      Rails.logger.error "Missing required parameter: code"
       flash[:alert] = "Google認証がキャンセルされました"
       redirect_to root_path and return
     end
 
     # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
     if (@user = login_from(provider))
-      # Rails.logger.debug "User logged in: #{@user.id}"
-      # binding.pry
       redirect_to tackles_path, success: "#{provider.titleize}アカウントでログインしました"
     else
       begin
-        # Rails.logger.debug "User not found, creating new user from #{provider}"
         # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
         signup_and_login(provider)
         redirect_to tackles_path, success: "#{provider.titleize}アカウントでログインしました"
       rescue => e
-        # Rails.logger.error "Error during login from #{provider}: #{e.message}"
         flash.now[:alert] = "#{provider.titleize}アカウントでのログインに失敗しました"
         redirect_to root_path
       end
@@ -40,7 +33,6 @@ class OauthsController < ApplicationController
 
   def auth_params
     params.permit(:code, :provider, :scope, :authuser, :prompt)
-    # params.permit(:code, :provider, :error, :state)
   end
 
   def signup_and_login(provider)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_015857) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_055509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -110,7 +110,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_015857) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "crypted_password", null: false
+    t.string "crypted_password"
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
