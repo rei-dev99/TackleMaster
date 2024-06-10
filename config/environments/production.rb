139d3f3699ae9e0c2ffe3bd59e0476809fb6d410
rei <idareiya123@gmail.com>
2024-06-10 15:10:34 +0900

    password_reset_production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tacklemaster_production"
 
   config.action_mailer.perform_caching = false
-
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
