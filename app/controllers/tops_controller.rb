21a7c97364aa828c67ed5868034c9ca2d117f96f
rei <idareiya123@gmail.com>
2024-06-11 13:22:32 +0900

    policy&terms_set

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,4 +2,8 @@ class TopsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index; end
+
+  def privacy_policy; end
+
+  def terms_of_service; end
 end
