e97b54c24a91f08170c778524ff2938d21b446ae
rei <idareiya123@gmail.com>
2024-06-11 13:30:14 +0900

    set_complete

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,5 @@
 class TopsController < ApplicationController
-  skip_before_action :require_login, only: %i[index]
+  skip_before_action :require_login, only: %i[index privacy_policy terms_of_service]
 
   def index; end
 
