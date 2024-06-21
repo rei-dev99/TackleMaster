615c86cd196f7516008e7886e5d9fe9ffefe2664
rei <idareiya123@gmail.com>
2024-06-21 21:56:07 +0900

    jsエラー

--- app/javascript/tackles_new.js
+++ app/javascript/tackles_new.js
@@ -1,7 +1,8 @@
-var button = document.getElementById("suggestion");
-button.addEventListener("click", function() {
-    // 要素を取得する
-    var message = document.getElementById("wait");
-    message.classList.remove('hidden');
-    this.classList.add("hidden");
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("suggestion");
+    button.addEventListener("click", function() {
+        var message = document.getElementById("wait");
+        message.classList.remove('hidden');
+        this.classList.add("hidden");
+    });
 });
