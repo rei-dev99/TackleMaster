3a2ae84b78b4dbec71c5d1113d048bb2ffae6536
rei <idareiya123@gmail.com>
2024-06-21 09:49:57 +0900

    バリデーション

--- app/models/fishing_gear.rb
+++ app/models/fishing_gear.rb
@@ -1,3 +1,11 @@
 class FishingGear < ApplicationRecord
   belongs_to :user
+
+  validates :fish_type, presence: true
+  validates :budget, presence: true
+  validates :location, presence: true
+  validates :fishing_type, presence: true
+  validates :tackle_type, presence: true
+  validates :tackle_maker, presence: true
+  validates :skill_level, presence: true
 end
