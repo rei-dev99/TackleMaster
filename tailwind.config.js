35eff1e9b38017f888e81f7bce2b10194afe8f0c
rei <idareiya123@gmail.com>
2024-06-22 14:09:38 +0900

    線の色

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require("esbuild");
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -14,6 +16,27 @@ module.exports = {
       'blue': '#1E90FF',
       'white': '#ffffff',
       'silver': '#C0C0C0',
+    },
+    borderColor: {
+      'black': '#000080',
+      'link': '#1E90FF',
+      'orange': '#FF7F50',
+      'blue': '#1E90FF',
+      'white': '#ffffff',
+      'silver': '#C0C0C0',
+    },
+    extend: {
+      keyframes: {
+        flashFade: {
+          "0%": { transform: "translateX(180px)", opacity: 0 },
+          "20%": { transform: "translateX(0)", opacity: 1 },
+          "80%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(180px)", opacity: 0 },
+        },
+      },
+      animation: {
+        flash: "flashFade 6.0s forwards",
+      },
     },
   },
 }
