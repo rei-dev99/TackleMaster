37af453520a583ee72c354a30c447be8d439c5b3
rei <idareiya123@gmail.com>
2024-06-23 11:42:23 +0900

    修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,15 +6,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to tackles_path, notice: t('user_sessions.create.success')
+      redirect_to tackles_path, notice: t("user_sessions.create.success")
     else
-      flash.now[:alert] = t('user_sessions.create.failure')
+      flash.now[:alert] = t("user_sessions.create.failure")
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, notice: t('user_sessions.destroy.success'), status: :see_other
+    redirect_to root_path, notice: t("user_sessions.destroy.success"), status: :see_other
   end
 end
