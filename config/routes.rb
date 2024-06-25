4e827f789bd811088df84729e42d9fbbbedd95d7
rei <idareiya123@gmail.com>
2024-06-25 15:18:08 +0900

    showルーディング追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   root "tops#index"
   resources :users
   resources :tackles do
-    resources :rods, only: %i[new create edit update destroy]
-    resources :reels, only: %i[new create edit update destroy]
-    resources :accesories, only: %i[new create edit update destroy]
+    resources :rods, only: %i[show new create edit update destroy]
+    resources :reels, only: %i[show new create edit update destroy]
+    resources :accesories, only: %i[show new create edit update destroy]
   end
   resources :password_resets, only: %i[new create edit update]
 
