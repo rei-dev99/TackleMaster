a793d6910334f25bf0876667d08be2fb691dc737
rei <idareiya123@gmail.com>
2024-06-25 10:39:43 +0900

    makerバリデーション追加

--- app/models/accesory.rb
+++ app/models/accesory.rb
@@ -3,6 +3,7 @@ class Accesory < ApplicationRecord
   has_many :tackles, through: :tackle_accesories
 
   validates :name, presence: true
+  validates :maker, presence: true
   validates :memo, length: { maximum: 500 }
 
   has_one_attached :image
--- app/models/rod.rb
+++ app/models/rod.rb
@@ -3,6 +3,7 @@ class Rod < ApplicationRecord
   has_many :tackles, through: :tackle_rods
 
   validates :name, presence: true
+  validates :maker, presence: true
   validates :memo, length: { maximum: 500 }
 
   has_one_attached :image
