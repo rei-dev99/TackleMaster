c3ebb5ac1b0a38cdaa2ac40a4dc9d7d7f6888f81
rei <idareiya123@gmail.com>
2024-07-06 08:05:09 +0900

    不要なマイグレーションファイル削除

--- db/migrate/20240614045751_add_user_id_to_fishing_plans.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToFishingPlans < ActiveRecord::Migration[7.1]
-  def change
-    add_column :fishing_plans, :user_id, :integer
-  end
-end
--- db/migrate/20240615042547_add_fish_type_to_fishing_plans.rb
+++
@@ -1,5 +0,0 @@
-class AddFishTypeToFishingPlans < ActiveRecord::Migration[7.1]
-  def change
-    add_column :fishing_plans, :fish_types, :string, null: false
-  end
-end
