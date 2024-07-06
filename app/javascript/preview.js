21cbef6ee5d62a64874574e90e9b21cb09b40d59
rei <idareiya123@gmail.com>
2024-07-07 07:59:38 +0900

    各フォームでプレビュー表示確認

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,22 +1,31 @@
-document.addEventListener('turbo:load', function(){
-    const postForm = document.getElementById('preview_item');
-    const previewList = document.getElementById('previews');
-    if (!postForm) return null;
-    const fileField = document.querySelector('input[type="file"][name="tackle[image]"]');
+document.addEventListener('turbo:load', function() {
+  const formIds = ['tackle_form', 'rod_form', 'reel_form', 'accessory_form'];
 
-    fileField.addEventListener('change', function(e){
-    const alreadyPreview = document.querySelector('.preview');
-    if (alreadyPreview) {
-      alreadyPreview.remove();
-    };
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-    const previewWrapper = document.createElement('div');
-    previewWrapper.setAttribute('class', 'preview');
-    const previewImage = document.createElement('img');
-    previewImage.setAttribute('class', 'preview-image');
-    previewImage.setAttribute('src', blob);
-    previewWrapper.appendChild(previewImage);
-    previewList.appendChild(previewWrapper);
-    });
+  formIds.forEach(id => {
+      const postForm = document.getElementById(id);
+      if (!postForm) return;
+
+      const fileField = postForm.querySelector('input[type="file"][name$="[image]"]');
+      const previewList = postForm.querySelector('#previews');
+
+      fileField.addEventListener('change', function(e) {
+        const alreadyPreview = postForm.querySelector('.preview');
+        if (alreadyPreview) {
+            alreadyPreview.remove();
+        }
+
+        const file = e.target.files[0];
+        const blob = window.URL.createObjectURL(file);
+
+        const previewWrapper = document.createElement('div');
+        previewWrapper.setAttribute('class', 'preview');
+
+        const previewImage = document.createElement('img');
+        previewImage.setAttribute('class', 'preview-image');
+        previewImage.setAttribute('src', blob);
+
+        previewWrapper.appendChild(previewImage);
+        previewList.appendChild(previewWrapper);
+      });
+  });
 });
