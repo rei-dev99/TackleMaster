566c204ed6d23fa0af38bc6d5480fac5661810a0
rei <idareiya123@gmail.com>
2024-07-07 15:08:16 +0900

    プレビュー表示js

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,31 +1,30 @@
 document.addEventListener('turbo:load', function() {
-  const formIds = ['tackle_form', 'rod_form', 'reel_form', 'accessory_form'];
+  const forms = document.querySelectorAll('form[id$="_form"]');
 
-  formIds.forEach(id => {
-      const postForm = document.getElementById(id);
-      if (!postForm) return;
+  forms.forEach(form => {
+    const fileField = form.querySelector('input[type="file"][name$="[image]"]');
+    if (!fileField) return;
 
-      const fileField = postForm.querySelector('input[type="file"][name$="[image]"]');
-      const previewList = postForm.querySelector('#previews');
+    const previewList = form.querySelector(`#previews_${form.id.split('_')[0]}`);
 
-      fileField.addEventListener('change', function(e) {
-        const alreadyPreview = postForm.querySelector('.preview');
-        if (alreadyPreview) {
-            alreadyPreview.remove();
-        }
+    fileField.addEventListener('change', function(e) {
+      const alreadyPreview = previewList.querySelector('.preview');
+      if (alreadyPreview) {
+        alreadyPreview.remove();
+      }
 
-        const file = e.target.files[0];
-        const blob = window.URL.createObjectURL(file);
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
 
-        const previewWrapper = document.createElement('div');
-        previewWrapper.setAttribute('class', 'preview');
+      const previewWrapper = document.createElement('div');
+      previewWrapper.setAttribute('class', 'preview');
 
-        const previewImage = document.createElement('img');
-        previewImage.setAttribute('class', 'preview-image');
-        previewImage.setAttribute('src', blob);
+      const previewImage = document.createElement('img');
+      previewImage.setAttribute('class', 'preview-image');
+      previewImage.setAttribute('src', blob);
 
-        previewWrapper.appendChild(previewImage);
-        previewList.appendChild(previewWrapper);
-      });
+      previewWrapper.appendChild(previewImage);
+      previewList.appendChild(previewWrapper);
+    });
   });
 });
