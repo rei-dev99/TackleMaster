92e1366a6b8b6fb8f768b04d7fcc92992d39e50b
rei <idareiya123@gmail.com>
2024-07-10 08:00:19 +0900

    独自ドメイン設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,4 +106,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << 'www.tacklemaster.net'
 end
