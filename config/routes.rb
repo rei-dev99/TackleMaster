873c7f07ec5084db8250bc91df5504a353e5be70
rei <idareiya123@gmail.com>
2024-07-14 07:44:30 +0900

    ルーディング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "tops#index"
-  resources :users
+  resources :users, only: %i[new create]
   resources :tackles do
     resources :rods, only: %i[show new create edit update destroy] do
       collection do
