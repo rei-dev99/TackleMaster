d8144df71b955013e5e7ca1039eed13ae50b267d
rei <idareiya123@gmail.com>
2024-07-18 14:21:54 +0900

    jsxファイル読み込み

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,11 @@
 // Entry point for the build script in your package.json
+// Turbo.session.drive = false  // ターボ無効
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./tackles_new"
 import "./preview"
 import "./scroll"
 import "./loading"
-import "./react/entrypoints/application"
+import "./react/entrypoints/scrollhint"
+import "./react/entrypoints/before_login_header"
+import "./react/entrypoints/before_login_footer"
