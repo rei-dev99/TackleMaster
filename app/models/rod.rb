4dd824349bce49844ba951061abf98d89fd1b512
rei <idareiya123@gmail.com>
2024-07-21 07:15:52 +0900

    回数変更

--- app/models/rod.rb
+++ app/models/rod.rb
@@ -8,7 +8,7 @@ class Rod < ApplicationRecord
 
   has_one_attached :image
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["name", "created_at", "updated_at", "memo", "id", "maker"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[name created_at updated_at memo id maker]
   end
 end
