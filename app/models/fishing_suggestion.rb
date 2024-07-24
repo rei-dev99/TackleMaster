a4c39ff18c7358374145065b0b92e7b0f79d09e2
rei <idareiya123@gmail.com>
2024-07-24 14:09:09 +0900

    モデル名変更

---
+++ app/models/fishing_suggestion.rb
@@ -0,0 +1,11 @@
+class FishingSuggestion < ApplicationRecord
+  belongs_to :user
+
+  validates :fish_type, presence: true
+  validates :budget, presence: true
+  validates :location, presence: true
+  validates :fishing_type, presence: true
+  validates :tackle_type, presence: true
+  validates :tackle_maker, presence: true
+  validates :skill_level, presence: true
+end
