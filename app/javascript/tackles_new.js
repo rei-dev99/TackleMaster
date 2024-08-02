9898c2d6440006c5c31a530b07d4b3de6c1db8f8
rei <idareiya123@gmail.com>
2024-08-02 11:28:12 +0900

    ローディング時の動き記述

--- app/javascript/tackles_new.js
+++ app/javascript/tackles_new.js
@@ -1,17 +1,21 @@
 document.addEventListener("turbo:load", function() {
     var button = document.getElementById("suggestion");
-    var form = document.getElementById("fishing-gear-form");
-
-    button.addEventListener("click", function() {
-        var message = document.getElementById("wait");
-        message.classList.remove('hidden');
-        this.classList.add("hidden");
+    var form = document.getElementById("fishing-suggestion-form");
+    var modal = document.getElementById("loading-modal");
+  
+    button.addEventListener("click", function(event) {
+      var message = document.getElementById("wait");
+      modal.classList.remove('hidden');
+      button.classList.add("hidden");
     });
-
+  
+    form.addEventListener("ajax:success", function() {
+      modal.classList.add('hidden');
+      button.classList.remove('hidden');
+    });
+  
     form.addEventListener("ajax:error", function() {
-        var message = document.getElementById("wait");
-        var button = document.getElementById("suggestion");
-        message.classList.add('hidden');
-        button.classList.remove('hidden');
+      modal.classList.add('hidden');
+      button.classList.remove('hidden');
     });
-});
+  });
